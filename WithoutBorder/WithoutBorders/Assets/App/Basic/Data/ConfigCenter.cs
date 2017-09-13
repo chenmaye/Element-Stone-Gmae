29af4bdb208223b54784a1117ef1e9dc24cd57f4
chenyiheng <chenyiheng>
2017-09-13 16:27:25 +0800

    without borders 工程文件上传

---
+++ WithoutBorder/WithoutBorders/Assets/App/Basic/Data/ConfigCenter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System;
+using ProtoBuf;
+
+public class ConfigCenter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
